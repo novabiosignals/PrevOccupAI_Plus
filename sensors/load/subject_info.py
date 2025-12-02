39f709e1a08ffa41a035b080843269d254a0eef3
SaraLMS <slm.santos@campus.fct.unl.pt>
2025-12-02 16:09:58 +0000

    changed variable and function names in sensors.load.subject_info

--- sensors/load/subject_info.py
+++ sensors/load/subject_info.py
@@ -4,9 +4,9 @@ Functions for loading the meta-data contained in 'subjects_info.csv'.
 Available Functions
 -------------------
 [Public]
-load_meta_data(...): loads the meta-data contained in subjects_info.csv into a pandas.DataFrame.
-get_muscleban_side(...): get the muscleban side based on the mac address
-get_expected_devices(...)
+load_participants_info(...): loads the meta-data contained in subjects_info.csv into a pandas.DataFrame.
+get_muscleban_side(...): get the muscleban side based on the mac address.
+get_participant_id(...): gets the subject id based on the device number and group.
 ------------------
 [Private]
 
@@ -15,16 +15,16 @@ get_expected_devices(...)
 # ------------------------------------------------------------------------------------------------------------------- #
 # imports
 # ------------------------------------------------------------------------------------------------------------------- #
-from typing import List
 import pandas as pd
 
 # internal imports
-from constants import MBAN_LEFT, MBAN_RIGHT, PHONE, WATCH
+from constants import MBAN_LEFT, MBAN_RIGHT
+from typing import Optional
 
 # ------------------------------------------------------------------------------------------------------------------- #
 # public functions
 # ------------------------------------------------------------------------------------------------------------------- #
-def load_meta_data():
+def load_participants_info() -> pd.DataFrame:
     """
     loads the meta-data contained in subjects_info.csv into a pandas.DataFrame.
     :return: DataFrame containing the meta-data
@@ -33,55 +33,37 @@ def load_meta_data():
     return pd.read_csv('participants_info.csv', sep=';', encoding='utf-8', index_col='subject_id')
 
 
-def get_muscleban_side(meta_data_df, mac_address):
+def get_muscleban_side(participants_info_df: pd.DataFrame, mac_address: str) -> Optional[str]:
     """
     Extracts the side of the muscleban from the meta_data_df based on the mac address of the device
-    :param meta_data_df: pd.DataFrame containing the subject meta-data contained in subjects_info.csv
+    :param participants_info_df: pd.DataFrame containing the subject meta-data contained in subjects_info.csv
     :param mac_address: str containing the mac address without the colons
     :return: str containing the muscleban side
     """
     # Search in mBAN_left column
-    if mac_address in meta_data_df[MBAN_LEFT].values:
+    if mac_address in participants_info_df[MBAN_LEFT].values:
         return MBAN_LEFT
 
     # Search in mBAN_right column
-    elif mac_address in meta_data_df[MBAN_RIGHT].values:
+    elif mac_address in participants_info_df[MBAN_RIGHT].values:
         return MBAN_RIGHT
 
     # If not found
     return None
 
 
-def get_expected_devices(meta_data_df: pd.DataFrame, group: str, device_num: str) -> List[str]:
-
-    # list with expected device - phone and watch are added manually as they are not on the metadata df
-    expected_devices = [PHONE, WATCH]
-
-    # Filter the DataFrame for the given group and device number
-    row = meta_data_df[(meta_data_df['group'] == group) & (meta_data_df['device_num'] == device_num)]
-
-    # get the mac address of the musclebans
-    mban_left = row.iloc[0][MBAN_LEFT]
-    mban_right = row.iloc[0][MBAN_RIGHT]
-
-    # muscleban mac addresses to the list
-    expected_devices.extend([mban_left, mban_right])
-
-    return expected_devices
-
-
-def get_id(meta_data_df: pd.DataFrame, device_num: str, group: str) -> str:
+def get_participant_id(participants_info_df: pd.DataFrame, device_num: str, group: str) -> str:
     """
-    Gets the subject id based on the device number and group
+    Gets the subject id based on the device number and group.
 
-    :param meta_data_df: DataFrame containing the participants info
+    :param participants_info_df: DataFrame containing the participants info
     :param device_num: device number to search for in the df
     :param group: group number to search for in the df
     :return: a string with the subject id
     """
 
     # Filter the DataFrame for the given group and device number
-    row = meta_data_df[(meta_data_df['group'] == int(group)) & (meta_data_df['device_num'] == device_num)]
+    row = participants_info_df[(participants_info_df['group'] == int(group)) & (participants_info_df['device_num'] == device_num)]
 
     # get the id which corresponds to the index of the row
     return str(row.index[0])
