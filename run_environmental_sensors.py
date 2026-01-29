98dc8eda23aac436b7cece4e104736e3ce0c9dde
SaraLMS <slmsantos01@gmail.com>
2026-01-29 19:00:30 +0000

    changed colors in activities plots and added data for subject 99 (environmental sensors)

--- run_environmental_sensors.py
+++ run_environmental_sensors.py
@@ -18,7 +18,7 @@ RERUN_OH_PROFILE = True
 # ------------------------------------------------------------------------------------------------------------------- #
 # file constants
 # ------------------------------------------------------------------------------------------------------------------- #
-DRIVE = 'D'
+DRIVE = 'E'
 OH_PROFILE_PATH = f"{DRIVE}:\\Backup PrevOccupAI_PLUS Data\\OH_profiles"
 PLOTS_OUTPUT_PATH = f"{DRIVE}:\\Backup PrevOccupAI_PLUS Data\\OH_plots"
 
--- sensors/visualize/human_activities.py
+++ sensors/visualize/human_activities.py
@@ -25,7 +25,7 @@ import os
 # internal imports
 from .plot_utils import handle_plot, get_weekday_name, add_percentage_labels
 from HAR.classifier import CLASS_WALK, CLASS_SIT, CLASS_STAND
-from .constants import RED, YELLOW, BLUE_STATE, GREEN, SALMON, GRAY
+from .constants import RED, YELLOW, BLUE_STATE, PALE_GREEN, SALMON, GRAY
 from OH_profile.constants import HAR_DISTRIBUTIONS_KEY, HAR_TIMELINE_KEY, HAR_STEPS_KEY, HAR_DISTANCE_KEY, HAR_NUM_STEPS_KEY
 from utils import create_dir
 # ------------------------------------------------------------------------------------------------------------------- #
@@ -42,7 +42,7 @@ SESSION_DT = 'session_dt'
 
 activity_colors = {
     WALKING_NAME: BLUE_STATE,
-    SITTING_NAME: GREEN,
+    SITTING_NAME: PALE_GREEN,
     STANDING_NAME: SALMON,
 }
 
